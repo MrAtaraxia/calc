34ac6a97fb903c2bb6544b7dc25e0e03b769dda2
Will <william.chnowski@gmail.com>
2021-04-10 08:47:51 -0500

    added calculator and requirements

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""
+Calculator Library Thing.
+"""
+
+
+def add(first_term, second_term):
+	return first_term + second_term
+
+
+def subtract(first_term, second_term):
+	return first_term - second_term
